10bf1e3c8864c126fe7b4e3a3f944606ebb93958
Van Phan Mac Air <pvphat25@gmail.com>
2025-01-06 22:02:42 -0500

    rewrite for better readability

--- back-end/main.go
+++ back-end/main.go
@@ -13,13 +13,14 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	http.Handle("/404", http.NotFoundHandler())
+
+	// middleware logger request, logger intercept the incoming req to the mux
+	http.Handle("/", logger.Logger(mux))
+
 	mux.HandleFunc("/home", handlers.Home)
 	mux.HandleFunc("/blog", handlers.Blog)
 	mux.HandleFunc("/contact", handlers.Contact)
-
-	http.Handle("/404", http.NotFoundHandler())
-
-	http.Handle("/", logger.Logger(mux))
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
