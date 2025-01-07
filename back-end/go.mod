5eae2b30d22e62e89ca5910dea4f5fe886fa7f01
Van Phan Mac Air <pvphat25@gmail.com>
2025-01-07 18:42:20 -0500

    add example templ for home

--- back-end/go.mod
+++ back-end/go.mod
@@ -1,3 +1,5 @@
 module vphatfla.com/vphatfla
 
 go 1.23.0
+
+require github.com/a-h/templ v0.3.819 // indirect
--- back-end/main.go
+++ back-end/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/a-h/templ"
 	"vphatfla.com/vphatfla/handlers"
+	"vphatfla.com/vphatfla/html"
 	"vphatfla.com/vphatfla/logger"
 )
 
 func main() {
-	log.Println("Backend Server Started!")
+	log.Println("Backend Server Started, listening on port 8000!")
 
 	mux := http.NewServeMux()
 
@@ -18,9 +20,13 @@ func main() {
 	// middleware logger request, logger intercept the incoming req to the mux
 	http.Handle("/", logger.Logger(mux))
 
-	mux.HandleFunc("/home", handlers.Home)
 	mux.HandleFunc("/blog", handlers.Blog)
 	mux.HandleFunc("/contact", handlers.Contact)
 
+	// test route for templ
+	h := html.Hello("Oppy")
+	mux.Handle("/hello", templ.Handler(h))
+	ho := html.Home()
+	mux.Handle("/home", templ.Handler(ho))
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
