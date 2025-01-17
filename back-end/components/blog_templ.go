3f37a449ae8cc5fc8a10cad87663a2e6274d3863
Van Phan Mac Air <pvphat25@gmail.com>
2025-01-17 16:32:44 -0500

    layout, size, and hx-swap

--- back-end/components/blog_templ.go
+++ back-end/components/blog_templ.go
@@ -29,7 +29,7 @@ func Blog() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div><p>Coming soon!</p></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"page-name\" hx-swap-oob=\"true\"><h2>Blog</h2></div><div><p>Coming soon!</p></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
