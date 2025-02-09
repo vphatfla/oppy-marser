e207fde179c56f92032e1e579dad6455d58ce0ea
Van Phan Mac Air <pvphat25@gmail.com>
2025-02-08 23:23:59 -0500

    add tf ignore and terraform for s3

---
+++ terraform/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "${var.app_name}-${var.environment}-website" 
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+  
+  index_document {
+    suffix = "index.html" 
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
+}
