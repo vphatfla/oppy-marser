6a4781afb7c66cbd12145bc2a998f7426194fa2b
Van Phan Mac Air <pvphat25@gmail.com>
2025-02-24 18:48:01 -0500

    modify security group

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "ec2_public" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
