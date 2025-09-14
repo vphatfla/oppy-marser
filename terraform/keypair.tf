8df6e422d932b712a31e17626ac8e9e55da1591d
Van Phan Mac Air <pvphat25@gmail.com>
2025-09-14 19:22:11 -0400

    clean up repo

--- terraform/keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "main" {
-  key_name   = "mac-keypair"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIaSsW1IJ2kQ3Kdpmh9B34o9BDkuqPp19pzWOSSWhS/ pvphat25@gmail.com"
-}
